5fa2b5df8b0cb7e0801a829438c747221bdf4e66
dan <dan@foohq.io>
2024-09-28 14:34:31 +0200

    clients/vessel: Add request decoders

--- clients/vessel/decoders.go
+++ clients/vessel/decoders.go
@@ -5,6 +5,27 @@ import (
 	"github.com/foojank/foojank/proto"
 )
 
+// TODO: parse root message (exported function!) DRY!
+
+func ParseCreateWorkerRequest(b []byte) error {
+	capMsg, err := capnp.Unmarshal(b)
+	if err != nil {
+		return err
+	}
+
+	message, err := proto.ReadRootMessage(capMsg)
+	if err != nil {
+		return err
+	}
+
+	_, err = message.Action().CreateWorker()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func ParseCreateWorkerResponse(b []byte) (uint64, error) {
 	capMsg, err := capnp.Unmarshal(b)
 	if err != nil {
@@ -17,6 +38,25 @@ func ParseCreateWorkerResponse(b []byte) (uint64, error) {
 	}
 
 	v, err := message.Response().CreateWorker()
+	if err != nil {
+		return 0, err
+	}
+
+	return v.Id(), nil
+}
+
+func ParseGetWorkerRequest(b []byte) (uint64, error) {
+	capMsg, err := capnp.Unmarshal(b)
+	if err != nil {
+		return 0, err
+	}
+
+	message, err := proto.ReadRootMessage(capMsg)
+	if err != nil {
+		return 0, err
+	}
+
+	v, err := message.Action().GetWorker()
 	if err != nil {
 		return 0, err
 	}
@@ -51,6 +91,30 @@ func ParseGetWorkerResponse(b []byte) (string, string, error) {
 	}
 
 	return serviceName, serviceID, nil
+}
+
+func ParseExecuteRequest(b []byte) ([]byte, error) {
+	capMsg, err := capnp.Unmarshal(b)
+	if err != nil {
+		return nil, err
+	}
+
+	message, err := proto.ReadRootMessage(capMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	v, err := message.Action().Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := v.Data()
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 func ParseExecuteResponse(b []byte) (int64, error) {
