f5bc1ef0b5a5f640cc4ca363a0158b76babcd8fb
dan <dan@foohq.io>
2024-10-26 11:41:47 +0200

    application/path: Add helper methods

--- internal/application/path/path.go
+++ internal/application/path/path.go
@@ -2,12 +2,32 @@ package path
 
 import (
 	"errors"
+	"path/filepath"
 	"strings"
 )
 
 type Path struct {
 	Repository string
 	FilePath   string
+}
+
+func (p Path) IsLocal() bool {
+	return p.Repository == ""
+}
+
+func (p Path) IsDir() bool {
+	return strings.HasSuffix(p.FilePath, "/")
+}
+
+func (p Path) Base() string {
+	return filepath.Base(p.FilePath)
+}
+
+func (p Path) String() string {
+	if p.IsLocal() {
+		return p.FilePath
+	}
+	return strings.Join([]string{p.Repository, p.FilePath}, ":")
 }
 
 func Parse(input string) (Path, error) {
@@ -25,6 +45,7 @@ func Parse(input string) (Path, error) {
 	parts := strings.SplitN(input, ":", 2)
 	repo := strings.TrimSpace(parts[0])
 	filePath := strings.TrimSpace(parts[1])
+	// TODO: cleanup filePath (it must always be an absolute path!)
 
 	if repo == "" || filePath == "" {
 		return Path{}, errors.New("repository and path cannot be empty")
