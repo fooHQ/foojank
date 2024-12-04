6943625bbf2b13362416c163997f7e8e523cd5ea
dan <dan@foohq.io>
2024-12-04 13:00:02 +0100

    client/commands/package/build: Add missing file

---
+++ internal/client/commands/package/build/build.go
@@ -0,0 +1,63 @@
+package build
+
+import (
+	"context"
+	"fmt"
+	"github.com/foohq/foojank/fzz"
+	"github.com/foohq/foojank/internal/client/actions"
+	"github.com/urfave/cli/v3"
+	"log/slog"
+	"path/filepath"
+)
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:      "build",
+		ArgsUsage: "<dir>",
+		Usage:     "Build a new package",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "name",
+				Usage: "set output name",
+			},
+		},
+		Action: action,
+	}
+}
+
+func action(ctx context.Context, c *cli.Command) error {
+	conf, err := actions.NewConfig(ctx, c)
+	if err != nil {
+		return err
+	}
+
+	logger := actions.NewLogger(ctx, conf)
+
+	if c.Args().Len() != 1 {
+		err := fmt.Errorf("command expects the following arguments: %s", c.ArgsUsage)
+		logger.Error(err.Error())
+		return err
+	}
+
+	return buildAction(logger)(ctx, c)
+}
+
+func buildAction(logger *slog.Logger) cli.ActionFunc {
+	return func(ctx context.Context, c *cli.Command) error {
+		src := c.Args().Get(0)
+		name := c.String("name")
+		if name == "" {
+			name = filepath.Base(src)
+		}
+
+		dst := fzz.NewFilename(name)
+		err := fzz.Build(src, dst)
+		if err != nil {
+			err := fmt.Errorf("cannot build a package: %v", err)
+			logger.Error(err.Error())
+			return err
+		}
+
+		return nil
+	}
+}
