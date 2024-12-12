d0d5245320815ad324452646f3728fa4048d1022
dan <dan@foohq.io>
2024-12-12 20:06:02 +0100

    Replace Makefile targets with devbox scripts

--- proto/capnp/generate.go
+++ proto/capnp/generate.go
@@ -1,3 +1,3 @@
 package capnp
 
-//go:generate capnp compile -I /tmp/go-capnp/std/ -ogo proto.capnp
+//go:generate devbox run -- capnp compile -I ./build/go-capnp/std/ -o ./build/go-capnp/capnpc-go/capnpc-go ./proto/capnp/proto.capnp
