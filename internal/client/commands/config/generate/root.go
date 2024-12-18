15861dd7f7ec326eb492828f749507e1ed57999e
dan <dan@foohq.io>
2024-12-18 13:02:50 +0100

    client/commands/config: Remove comments

--- internal/client/commands/config/generate/root.go
+++ internal/client/commands/config/generate/root.go
@@ -9,36 +9,6 @@ import (
 	"github.com/foohq/foojank/internal/client/commands/config/generate/server"
 )
 
-// Usage:
-// # Generate commands print yaml to stdout
-// config generate seed
-
-// config generate server <seed-file>
-
-// # Generate manager configuration file.
-// # This also creates a new USER nkey and JWT signed by a selected ACCOUNT.
-// # This configuration is meant to be used with foojank client application.
-// config generate client <file>
-
-// # Generate agent configuration file.
-// # This also creates a new USER nkey and JWT signed by a selected ACCOUNT.
-// # This configuration is meant to be used with vessel agent.
-// config generate agent
-
-// # For debugging only.
-// config generate --type=creds <seed-file>
-// TODO: use https://pkg.go.dev/github.com/nats-io/jwt/v2#DecorateJWT
-
-// # Generate and add to a seed file another account.
-// # Requires reloading the server configuration.
-// config account(s) add
-// config account(s) delete
-// config account(s) import
-
-// # Add another operator (maybe in the future!).
-// config operator(s) add
-// config operator(s) import
-
 func NewCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "generate",
--- internal/client/commands/config/root.go
+++ internal/client/commands/config/root.go
@@ -7,36 +7,6 @@ import (
 	"github.com/foohq/foojank/internal/client/commands/config/generate"
 )
 
-// Usage:
-// # Generate commands print yaml to stdout
-// config create > seed.yaml
-
-// config generate --type=server <seed-file>
-
-// # Generate manager configuration file.
-// # This also creates a new USER nkey and JWT signed by a selected ACCOUNT.
-// # This configuration is meant to be used with foojank client application.
-// config generate --type=client <seed-file>
-
-// # Generate agent configuration file.
-// # This also creates a new USER nkey and JWT signed by a selected ACCOUNT.
-// # This configuration is meant to be used with vessel agent.
-// config generate --type=agent <seed-file>
-
-// # For debugging only.
-// config generate --type=creds <seed-file>
-// TODO: use https://pkg.go.dev/github.com/nats-io/jwt/v2#DecorateJWT
-
-// # Generate and add to a seed file another account.
-// # Requires reloading the server configuration.
-// config account(s) add
-// config account(s) delete
-// config account(s) import
-
-// # Add another operator (maybe in the future!).
-// config operator(s) add
-// config operator(s) import
-
 func NewCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "config",
