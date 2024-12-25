0d2d8f253eec6e8c6dc354794986fbced5f186ea
dan <dan@foohq.io>
2024-12-25 13:19:49 +0100

    client/path: Fix linter error (add _test suffix to test package name)

--- internal/client/path/path_test.go
+++ internal/client/path/path_test.go
@@ -1,30 +1,32 @@
-package path
+package path_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/foohq/foojank/internal/client/path"
 )
 
 func TestParse(t *testing.T) {
 	tests := []struct {
 		input    string
-		expected Path
+		expected path.Path
 		err      bool
 	}{
-		{"myrepo:/path/to/file", Path{"myrepo", "/path/to/file"}, false},
-		{"anotherrepo:/another/path", Path{"anotherrepo", "/another/path"}, false},
-		{"/path/to/file", Path{"", "/path/to/file"}, false},
-		{"filename", Path{"", "filename"}, false},
-		{"repo:", Path{}, true},
-		{":/path/to/file", Path{}, true},
-		{" : /path/to/file", Path{}, true},
-		{"repo: ", Path{"repo", ""}, true},
-		{" ", Path{}, true},
+		{"myrepo:/path/to/file", path.Path{"myrepo", "/path/to/file"}, false},
+		{"anotherrepo:/another/path", path.Path{"anotherrepo", "/another/path"}, false},
+		{"/path/to/file", path.Path{"", "/path/to/file"}, false},
+		{"filename", path.Path{"", "filename"}, false},
+		{"repo:", path.Path{}, true},
+		{":/path/to/file", path.Path{}, true},
+		{" : /path/to/file", path.Path{}, true},
+		{"repo: ", path.Path{"repo", ""}, true},
+		{" ", path.Path{}, true},
 	}
 
 	for _, test := range tests {
-		result, err := Parse(test.input)
+		result, err := path.Parse(test.input)
 		if test.err {
 			assert.NotNil(t, err)
 			continue
