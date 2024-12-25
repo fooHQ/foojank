ae69a9e82893a0cc8ca950dd32e654b9a2b4363d
dan <dan@foohq.io>
2024-12-25 12:15:19 +0100

    runscript/actions: Fix linter error (unused parameter)

--- internal/runscript/actions/actions.go
+++ internal/runscript/actions/actions.go
@@ -8,7 +8,7 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-func CommandNotFound(ctx context.Context, c *cli.Command, s string) {
+func CommandNotFound(_ context.Context, c *cli.Command, s string) {
 	err := fmt.Errorf("command '%s' does not exist", s)
 	_, _ = fmt.Fprintf(os.Stderr, "%s: %v\n", c.FullName(), err.Error())
 	os.Exit(1)
