935d777b1ffa006306d08e392c4e6209574bcaf0
dan <dan@foohq.io>
2025-01-18 15:33:55 +0100

    devbox: Change build script for capnpc-go

--- proto/capnp/generate.go
+++ proto/capnp/generate.go
@@ -1,3 +1,3 @@
 package capnp
 
-//go:generate devbox run -- capnp compile -I ./build/go-capnp/std/ -o ./build/go-capnp/capnpc-go/capnpc-go ./proto/capnp/proto.capnp
+//go:generate devbox run -- capnp compile -I ./build/go-capnp/std/ -o ./build/capnpc-go ./proto/capnp/proto.capnp
