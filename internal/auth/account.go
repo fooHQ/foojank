90f8f30e62663703ee931169743cde40eaf39c90
dan <dan@foohq.io>
2025-01-20 21:17:49 +0100

    Add auth package
    This is a new destination for JWT/NKey related functions from the
    config package.

---
+++ internal/auth/account.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nkeys"
+)
+
+type Account struct {
+	JWT        string
+	Key        string
+	SigningKey string
+}
+
+func NewAccount(name string, operatorSignKey []byte, enableJetstream bool) (*Account, error) {
+	operatorSignKeyPair, err := nkeys.FromSeed(operatorSignKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode operator's signing key: %w", err)
+	}
+
+	keyPair, err := nkeys.CreateAccount()
+	if err != nil {
+		return nil, fmt.Errorf("cannot generate account's key: %w", err)
+	}
+
+	pubKey, err := keyPair.PublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get account's public key: %w", err)
+	}
+
+	signKeyPair, err := nkeys.CreateAccount()
+	if err != nil {
+		return nil, fmt.Errorf("cannot generate account's key: %w", err)
+	}
+
+	signPubKey, err := signKeyPair.PublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get account's signing public key: %w", err)
+	}
+
+	claims := jwt.NewAccountClaims(pubKey)
+	claims.Name = name
+	if enableJetstream {
+		claims.Limits.JetStreamLimits.DiskStorage = -1
+		claims.Limits.JetStreamLimits.MemoryStorage = -1
+	}
+	claims.SigningKeys.Add(signPubKey)
+	claimsEnc, err := claims.Encode(operatorSignKeyPair)
+	if err != nil {
+		return nil, fmt.Errorf("cannot encode and sign account claims: %w", err)
+	}
+
+	keySeed, err := keyPair.Seed()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get a seed of account's key: %w", err)
+	}
+
+	signKeySeed, err := signKeyPair.Seed()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get a seed of account's key: %w", err)
+	}
+
+	return &Account{
+		JWT:        claimsEnc,
+		Key:        string(keySeed),
+		SigningKey: string(signKeySeed),
+	}, nil
+}
---
+++ internal/auth/user.go
@@ -0,0 +1,110 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nkeys"
+)
+
+type User struct {
+	JWT string
+	Key string
+}
+
+func NewUserManager(name, accountPubKey string, accountSigningKey []byte) (*User, error) {
+	accountSignKeyPair, err := nkeys.FromSeed(accountSigningKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode account's signing key: %w", err)
+	}
+
+	keyPair, err := nkeys.CreateUser()
+	if err != nil {
+		return nil, fmt.Errorf("cannot generate user key: %w", err)
+	}
+
+	pubKey, err := keyPair.PublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get user's public key: %w", err)
+	}
+
+	claims := jwt.NewUserClaims(pubKey)
+	claims.Name = name
+	claims.IssuerAccount = accountPubKey
+	claimsEnc, err := claims.Encode(accountSignKeyPair)
+	if err != nil {
+		return nil, fmt.Errorf("cannot encode and sign user claims: %w", err)
+	}
+
+	keySeed, err := keyPair.Seed()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get a seed of user's key-pair: %w", err)
+	}
+
+	return &User{
+		JWT: claimsEnc,
+		Key: string(keySeed),
+	}, nil
+}
+
+func NewUserAgent(name, accountPubKey string, accountSigningKey []byte) (*User, error) {
+	accountSignKeyPair, err := nkeys.FromSeed(accountSigningKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode account's signing key: %w", err)
+	}
+
+	keyPair, err := nkeys.CreateUser()
+	if err != nil {
+		return nil, fmt.Errorf("cannot generate user key: %w", err)
+	}
+
+	pubKey, err := keyPair.PublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get user's public key: %w", err)
+	}
+
+	claims := jwt.NewUserClaims(pubKey)
+	claims.Name = name
+	claims.IssuerAccount = accountPubKey
+	claims.Sub = jwt.Permission{
+		Allow: []string{
+			fmt.Sprintf("_INBOX_%s.>", name),
+			"$SRV.PING",
+			fmt.Sprintf("$SRV.PING.%s", name),
+			fmt.Sprintf("$SRV.PING.%s.*", name),
+			"$SRV.INFO",
+			fmt.Sprintf("$SRV.INFO.%s", name),
+			fmt.Sprintf("$SRV.INFO.%s.*", name),
+			"$SRV.STATS",
+			fmt.Sprintf("$SRV.STATS.%s", name),
+			fmt.Sprintf("$SRV.STATS.%s.*", name),
+			fmt.Sprintf("%s.RPC", name),
+			fmt.Sprintf("%s.*.DATA", name),
+			fmt.Sprintf("%s.*.STDIN", name),
+		},
+	}
+	claims.Pub = jwt.Permission{
+		Allow: []string{
+			fmt.Sprintf("$JS.API.STREAM.INFO.OBJ_%s", name),
+			fmt.Sprintf("$JS.API.DIRECT.GET.OBJ_%s.$O.%s.M.*", name, name),
+			fmt.Sprintf("$JS.API.CONSUMER.CREATE.OBJ_%s.*.$O.%s.C.*", name, name),
+			fmt.Sprintf("$JS.API.CONSUMER.DELETE.OBJ_%s.*", name),
+			fmt.Sprintf("%s.*.STDOUT", name),
+			"_INBOX.>",
+		},
+	}
+	claimsEnc, err := claims.Encode(accountSignKeyPair)
+	if err != nil {
+		return nil, fmt.Errorf("cannot encode and sign user claims: %w", err)
+	}
+
+	keySeed, err := keyPair.Seed()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get a seed of user's key-pair: %w", err)
+	}
+
+	return &User{
+		JWT: claimsEnc,
+		Key: string(keySeed),
+	}, nil
+}
