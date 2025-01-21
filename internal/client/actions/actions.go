f18623afcd1050cbb754a73cbce149378ea1ac9d
dan <dan@foohq.io>
2025-01-21 14:16:58 +0100

    client/actions: Rename config import alias

--- internal/client/actions/actions.go
+++ internal/client/actions/actions.go
@@ -8,26 +8,25 @@ import (
 	"github.com/urfave/cli/v3"
 
 	"github.com/foohq/foojank/internal/client/flags"
-	// TODO: rename import!
-	configv2 "github.com/foohq/foojank/internal/config/v2"
+	"github.com/foohq/foojank/internal/config/v2"
 )
 
 // NewClientConfig TODO: RENAME ME!
-func NewClientConfig(_ context.Context, c *cli.Command) (*configv2.Config, error) {
-	confDefault, err := configv2.NewDefault()
+func NewClientConfig(_ context.Context, c *cli.Command) (*config.Config, error) {
+	confDefault, err := config.NewDefault()
 	if err != nil {
 		err = fmt.Errorf("cannot create a new configuration: %w", err)
 		return nil, err
 	}
 
 	file := c.String(flags.Config)
-	confFile, err := configv2.ParseFile(file)
+	confFile, err := config.ParseFile(file)
 	if err != nil && !os.IsNotExist(err) {
 		err = fmt.Errorf("cannot parse configuration file '%s': %w", file, err)
 		return nil, err
 	}
 
-	confFlags, err := configv2.ParseFlags(func(name string) (any, bool) {
+	confFlags, err := config.ParseFlags(func(name string) (any, bool) {
 		return c.Value(name), c.IsSet(name)
 	})
 	if err != nil {
@@ -35,6 +34,6 @@ func NewClientConfig(_ context.Context, c *cli.Command) (*configv2.Config, error
 		return nil, err
 	}
 
-	result := configv2.Merge(confDefault, confFile, confFlags)
+	result := config.Merge(confDefault, confFile, confFlags)
 	return result, nil
 }
