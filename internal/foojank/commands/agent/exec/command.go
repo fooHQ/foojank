d78e7e98e6e68cacaa6c8197d821788a8a8bc574
dan <dan@foohq.io>
2025-05-24 17:58:10 +0200

    vessel: Read package from a repository
    Worker has access to an instance of repository which wraps natsfs.

--- internal/foojank/commands/agent/exec/command.go
+++ internal/foojank/commands/agent/exec/command.go
@@ -251,8 +251,7 @@ func execAction(logger *slog.Logger, vesselCli *vessel.Client, codebaseCli *code
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			fullPath := "nats://" + pkgExecPath
-			code, err := vesselCli.Execute(ctx, worker, fullPath, scriptArgs, stdinCh, stdoutCh)
+			code, err := vesselCli.Execute(ctx, worker, pkgExecPath, scriptArgs, stdinCh, stdoutCh)
 			if err != nil && !errors.Is(err, context.Canceled) {
 				err := fmt.Errorf("execute request failed: %w", err)
 				logger.ErrorContext(ctx, err.Error())
