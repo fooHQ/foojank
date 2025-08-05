6c49f0fe9f5a0148b3153177af9fd5f48fb95e77
dan <dan@foohq.io>
2025-08-05 16:17:16 +0200

    vessel/connector: Rewrite connector
    The service instantiates consumer -> decoder pipeline and writes
    mesages to its output channel.
    
    Joined service consuming the messages can reply to messages using
    Reply method which writes data to encoder -> publisher pipeline.

--- internal/vessel/connector/message.go
+++ internal/vessel/connector/message.go
@@ -1,25 +1,7 @@
 package connector
 
-import "github.com/nats-io/nats.go/micro"
+import (
+	"github.com/foohq/foojank/internal/vessel/decoder"
+)
 
-type Message struct {
-	req micro.Request
-}
-
-func NewMessage(req micro.Request) Message {
-	return Message{
-		req: req,
-	}
-}
-
-func (m Message) Data() []byte {
-	return m.req.Data()
-}
-
-func (m Message) Reply(data []byte) error {
-	return m.req.Respond(data)
-}
-
-func (m Message) ReplyError(code string, description string, data []byte) error {
-	return m.req.Error(code, description, data)
-}
+type Message = decoder.Message
--- internal/vessel/connector/service_test.go
+++ internal/vessel/connector/service_test.go
@@ -2,58 +2,121 @@ package connector_test
 
 import (
 	"context"
+	"crypto/rand"
+	"errors"
+	"fmt"
 	"sync"
 	"testing"
-	"time"
 
+	"github.com/nats-io/nats.go/jetstream"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/foohq/foojank/internal/testutils"
 	"github.com/foohq/foojank/internal/vessel/connector"
+	"github.com/foohq/foojank/proto"
 )
 
+// TestService has the following steps:
+// 1. publish messages to subject.
+// 2. start the service
+// 3. check that all messages were delivered
+// 4. respond to messages
+// 5. check that replies were published (requires another consumer)
 func TestService(t *testing.T) {
-	var rpcSubject = "rpc"
-	var reqData = []byte("_data_")
-	var respData = []byte("_resp_data_")
+	streamName := fmt.Sprintf("TEST-STREAM-%s", rand.Text())
+	consumerName := fmt.Sprintf("TEST-CONSUMER-%s", rand.Text())
+	subjectName := fmt.Sprintf("TEST.COMMANDS-%s", rand.Text())
+	srv, js := testutils.NewJetStreamConnection(t)
 
-	outputCh := make(chan connector.Message)
-	_, nc := testutils.NewNatsServerAndConnection(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	_, err := js.CreateStream(ctx, jetstream.StreamConfig{
+		Name:     streamName,
+		Subjects: []string{subjectName},
+	})
+	require.NoError(t, err)
 
+	_, err = js.CreateConsumer(ctx, streamName, jetstream.ConsumerConfig{
+		Durable:       consumerName,
+		DeliverPolicy: jetstream.DeliverLastPolicy,
+		AckPolicy:     jetstream.AckExplicitPolicy,
+	})
+	require.NoError(t, err)
+
+	requests := []any{
+		proto.CreateJobRequest{
+			Command: "exec",
+			Args:    []string{"arg1", "arg2"},
+			Env:     []string{"TEST", "hello"},
+		},
+		proto.CancelJobRequest{
+			JobID: "job-123",
+		},
+	}
+	replies := []any{
+		proto.CreateJobResponse{
+			JobID:         "job-123",
+			StdinSubject:  "stdin-subject",
+			StdoutSubject: "stdout-subject",
+			Error:         errors.New("some error"),
+		},
+		proto.CancelJobResponse{
+			Error: errors.New("some error"),
+		},
+	}
+
+	for _, req := range requests {
+		b, err := proto.Marshal(req)
+		require.NoError(t, err)
+
+		_, err = js.Publish(ctx, subjectName, b)
+		require.NoError(t, err)
+	}
+
+	outputCh := make(chan connector.Message, len(requests))
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		err := connector.New(connector.Arguments{
-			Name:       "test",
-			Version:    "0.0.1",
-			Metadata:   nil,
-			RPCSubject: rpcSubject,
-			Connection: nc,
-			OutputCh:   outputCh,
+			Servers:  []string{srv.ClientURL()},
+			Stream:   streamName,
+			Consumer: consumerName,
+			Subject:  subjectName,
+			OutputCh: outputCh,
 		}).Start(ctx)
 		assert.NoError(t, err)
 	}()
 
-	time.Sleep(1 * time.Second)
+	for i, req := range requests {
+		msg := <-outputCh
+		require.NoError(t, msg.Ack())
 
-	{
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			msg := <-outputCh
-			assert.Equal(t, reqData, msg.Data())
+		data := msg.Data()
+		require.IsType(t, req, data)
+		require.Equal(t, req, data)
 
-			err := msg.Reply(respData)
-			assert.NoError(t, err)
-		}()
-
-		resp, err := nc.Request(rpcSubject, reqData, 2*time.Second)
+		err := msg.Reply(ctx, replies[i])
 		require.NoError(t, err)
-		require.Equal(t, respData, resp.Data)
+	}
+
+	c, err := js.OrderedConsumer(ctx, streamName, jetstream.OrderedConsumerConfig{})
+	require.NoError(t, err)
+
+	batch, err := c.FetchNoWait(len(requests) + len(replies))
+	require.NoError(t, err)
+
+	messages := append(requests, replies...)
+	var i int
+	for msg := range batch.Messages() {
+		if msg == nil {
+			break
+		}
+		actual, err := proto.Unmarshal(msg.Data())
+		require.NoError(t, err)
+		require.Equal(t, messages[i], actual)
+		i++
 	}
 
 	cancel()
