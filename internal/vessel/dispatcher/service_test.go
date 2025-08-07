147504880becec5832af6d1b6e4294bb205f8b35
dan <dan@foohq.io>
2025-08-07 12:07:17 +0200

    vessel: Add dispatcher service
    The service processes job requests and forwards messages to
    running workers.

---
+++ internal/vessel/dispatcher/service_test.go
@@ -0,0 +1,19 @@
+package dispatcher
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"github.com/foohq/foojank/internal/testutils"
+)
+
+func TestService(t *testing.T) {
+
+	workerID := rand.Text()
+	streamName := fmt.Sprintf("TEST-STREAM-%s", rand.Text())
+	stdinName := fmt.Sprintf("TEST-STDIN-%s", rand.Text())
+	stdoutName := fmt.Sprintf("TEST-STDOUT-%s", rand.Text())
+	updateName := fmt.Sprintf("TEST-UPDATE-%s", rand.Text())
+	_, js := testutils.NewJetStreamConnection(t)
+}
