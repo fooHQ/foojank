93959bc76481de279314f4b882fc0bef0b05ecd3
dan <dan@foohq.io>
2025-08-30 15:09:17 +0200

    clients/codebase: Accept agent configuration via arguments
    The arguments are passed to devbox script via environment variables. The
    method has been refactored to accomodate sprawling number of options by
    adding a struct.

--- clients/codebase/client.go
+++ clients/codebase/client.go
@@ -48,29 +48,120 @@ func (c *Client) RunscriptConfigFile() string {
 	return filepath.Join(c.baseDir, "internal", "runscript", "config", "config.go")
 }
 
-func (c *Client) BuildAgent(ctx context.Context, os, arch string, production bool, tags []string) (string, string, error) {
+type BuildAgentOptions struct {
+	OS         string
+	Arch       string
+	Production bool
+	Tags       []string
+	Config     BuildAgentConfig
+}
+
+type BuildAgentConfig struct {
+	ID                           string
+	Server                       string
+	UserJWT                      string
+	UserKey                      string
+	CACertificate                string
+	Stream                       string
+	Consumer                     string
+	InboxPrefix                  string
+	ObjectStoreName              string
+	SubjectApiWorkerStartT       string
+	SubjectApiWorkerStopT        string
+	SubjectApiWorkerWriteStdinT  string
+	SubjectApiWorkerWriteStdoutT string
+	SubjectApiWorkerStatusT      string
+	SubjectApiConnInfoT          string
+	SubjectApiReplyT             string
+}
+
+func (c *BuildAgentConfig) ToFlags() string {
+	ldFlags := []string{
+		"-X main.ID=%s",
+		"-X main.Server=%s",
+		"-X main.UserJWT=%s",
+		"-X main.UserKey=%s",
+		"-X main.CACertificate=%s",
+		"-X main.Stream=%s",
+		"-X main.Consumer=%s",
+		"-X main.InboxPrefix=%s",
+		"-X main.ObjectStoreName=%s",
+		"-X main.SubjectApiWorkerStartT=%s",
+		"-X main.SubjectApiWorkerStopT=%s",
+		"-X main.SubjectApiWorkerWriteStdinT=%s",
+		"-X main.SubjectApiWorkerWriteStdoutT=%s",
+		"-X main.SubjectApiWorkerStatusT=%s",
+		"-X main.SubjectApiConnInfoT=%s",
+		"-X main.SubjectApiReplyT=%s",
+	}
+	flags := []string{
+		"-race",
+		fmt.Sprintf("-ldflags=\"%s\"", strings.Join(ldFlags, " ")),
+	}
+	return fmt.Sprintf(strings.Join(flags, " "),
+		c.ID,
+		c.Server,
+		c.UserJWT,
+		c.UserKey,
+		c.CACertificate,
+		c.Stream,
+		c.Consumer,
+		c.InboxPrefix,
+		c.ObjectStoreName,
+		c.SubjectApiWorkerStartT,
+		c.SubjectApiWorkerStopT,
+		c.SubjectApiWorkerWriteStdinT,
+		c.SubjectApiWorkerWriteStdoutT,
+		c.SubjectApiWorkerStatusT,
+		c.SubjectApiConnInfoT,
+		c.SubjectApiReplyT,
+	)
+}
+
+func (c *Client) BuildAgent(ctx context.Context, opts BuildAgentOptions) (string, string, error) {
 	err := c.baseDirExists()
 	if err != nil {
 		return "", "", err
 	}
 
 	script := "build-agent-dev"
-	if production {
+	if opts.Production {
 		script = "build-agent-prod"
 	}
+
 	output := filepath.Join(c.BuildDir(), nuid.Next())
-	if os == "windows" {
+	if opts.OS == "windows" {
 		output += ".exe"
 	}
-	result, err := c.devboxRun(ctx, script, map[string]string{
-		"GOOS":   os,
-		"GOARCH": arch,
-		"OUTPUT": output,
-		"TAGS":   strings.Join(tags, " "),
-	})
+
+	env := map[string]string{
+		"GOOS":                              opts.OS,
+		"GOARCH":                            opts.Arch,
+		"OUTPUT":                            output,
+		"TAGS":                              strings.Join(opts.Tags, " "),
+		"ID":                                opts.Config.ID,
+		"SERVER":                            opts.Config.Server,
+		"USER_JWT":                          opts.Config.UserJWT,
+		"USER_KEY":                          opts.Config.UserKey,
+		"CA_CERTIFICATE":                    opts.Config.CACertificate,
+		"STREAM":                            opts.Config.Stream,
+		"CONSUMER":                          opts.Config.Consumer,
+		"INBOX_PREFIX":                      opts.Config.InboxPrefix,
+		"OBJECT_STORE_NAME":                 opts.Config.ObjectStoreName,
+		"SUBJECT_API_WORKER_START_T":        opts.Config.SubjectApiWorkerStartT,
+		"SUBJECT_API_WORKER_STOP_T":         opts.Config.SubjectApiWorkerStopT,
+		"SUBJECT_API_WORKER_WRITE_STDIN_T":  opts.Config.SubjectApiWorkerWriteStdinT,
+		"SUBJECT_API_WORKER_WRITE_STDOUT_T": opts.Config.SubjectApiWorkerWriteStdoutT,
+		"SUBJECT_API_WORKER_STATUS_T":       opts.Config.SubjectApiWorkerStatusT,
+		"SUBJECT_API_CONN_INFO_T":           opts.Config.SubjectApiConnInfoT,
+		"SUBJECT_API_REPLY_T":               opts.Config.SubjectApiReplyT,
+	}
+
+	result, err := c.devboxRun(ctx, script, env)
 	if err != nil {
 		return "", result, err
 	}
+
 	return output, result, nil
 }
 
