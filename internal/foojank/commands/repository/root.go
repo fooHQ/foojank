aff57271b817fe14a83b4158e1e4fe6c5d25d807
dan <dan@foohq.io>
2025-09-02 17:34:16 +0200

    foojank/commands/repository: Remove create, destroy commands

--- internal/foojank/commands/repository/root.go
+++ internal/foojank/commands/repository/root.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/foohq/foojank/internal/foojank/actions"
 	"github.com/foohq/foojank/internal/foojank/commands/repository/copy"
-	"github.com/foohq/foojank/internal/foojank/commands/repository/create"
-	"github.com/foohq/foojank/internal/foojank/commands/repository/destroy"
 	"github.com/foohq/foojank/internal/foojank/commands/repository/list"
 	"github.com/foohq/foojank/internal/foojank/commands/repository/remove"
 )
@@ -16,8 +14,6 @@ func NewCommand() *cli.Command {
 		Name:  "repository",
 		Usage: "Manage repositories",
 		Commands: []*cli.Command{
-			create.NewCommand(),
-			destroy.NewCommand(),
 			list.NewCommand(),
 			copy.NewCommand(),
 			remove.NewCommand(),
