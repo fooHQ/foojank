1aeda0710ff975d23a2e660a5b9c23d9294242b0
dan <dan@foohq.io>
2025-10-04 08:31:52 +0200

    foojank/commands/agent: Add logs command

---
+++ internal/foojank/commands/agent/logs/command.go
@@ -0,0 +1,188 @@
+package logs
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v3"
+
+	"github.com/foohq/foojank/clients/server"
+	"github.com/foohq/foojank/clients/vessel"
+	"github.com/foohq/foojank/internal/config"
+	"github.com/foohq/foojank/internal/foojank/actions"
+	"github.com/foohq/foojank/internal/foojank/flags"
+	"github.com/foohq/foojank/internal/foojank/formatter"
+	jsonformatter "github.com/foohq/foojank/internal/foojank/formatter/json"
+	tableformatter "github.com/foohq/foojank/internal/foojank/formatter/table"
+	"github.com/foohq/foojank/internal/log"
+)
+
+const (
+	FlagFormat           = "format"
+	FlagServer           = flags.Server
+	FlagUserJWT          = flags.UserJWT
+	FlagUserKey          = flags.UserKey
+	FlagTLSCACertificate = flags.TLSCACertificate
+	FlagDataDir          = flags.DataDir
+)
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:      "logs",
+		ArgsUsage: "<agent-id>",
+		Usage:     "Display all messages in agent's stream",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  FlagFormat,
+				Usage: "set output format",
+				Value: "table",
+			},
+			&cli.StringSliceFlag{
+				Name:  FlagServer,
+				Usage: "set server URL",
+			},
+			&cli.StringFlag{
+				Name:  FlagUserJWT,
+				Usage: "set user JWT token",
+			},
+			&cli.StringFlag{
+				Name:  FlagUserKey,
+				Usage: "set user secret key",
+			},
+			&cli.StringFlag{
+				Name:  FlagTLSCACertificate,
+				Usage: "set TLS CA certificate",
+			},
+			&cli.StringFlag{
+				Name:  FlagDataDir,
+				Usage: "set path to a data directory",
+			},
+		},
+		Action:       action,
+		OnUsageError: actions.UsageError,
+	}
+}
+
+func action(ctx context.Context, c *cli.Command) error {
+	conf, err := actions.NewConfig(ctx, c)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%s: invalid configuration: %v\n", c.FullName(), err)
+		return err
+	}
+
+	err = validateConfiguration(conf)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%s: invalid configuration: %v\n", c.FullName(), err)
+		return err
+	}
+
+	srv, err := server.New(conf.Client.Server, *conf.Client.UserJWT, *conf.Client.UserKey, *conf.Client.TLSCACertificate)
+	if err != nil {
+		log.Error(ctx, "Cannot connect to the server: %v", err)
+		return err
+	}
+
+	if c.Args().Len() < 1 {
+		log.Error(ctx, "Command expects the following arguments: %s", c.ArgsUsage)
+		return err
+	}
+
+	client := vessel.New(srv)
+
+	agentID := c.Args().First()
+	format := c.String(FlagFormat)
+
+	msgs, err := client.ListMessages(ctx, agentID, nil)
+	if err != nil {
+		log.Error(ctx, "Cannot get a list of messages: %v", err)
+		return err
+	}
+
+	err = formatOutput(os.Stdout, format, msgs)
+	if err != nil {
+		log.Error(ctx, "Cannot write formatted output: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func formatOutput(w io.Writer, format string, data []vessel.Message) error {
+	table := formatter.NewTable([]string{
+		"message_id",
+		"subject",
+		"received",
+	})
+	for _, msg := range data {
+		table.AddRow([]string{
+			msg.ID,
+			msg.Subject,
+			formatTime(msg.Received),
+		})
+	}
+
+	var f formatter.Formatter
+	switch format {
+	case "json":
+		f = jsonformatter.New()
+	case "table":
+		f = tableformatter.New()
+	default:
+		f = tableformatter.New()
+	}
+
+	err := f.Write(w, table)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func formatTime(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func validateConfiguration(conf *config.Config) error {
+	if conf.LogLevel == nil {
+		return errors.New("log level not configured")
+	}
+
+	if conf.NoColor == nil {
+		return errors.New("no color not configured")
+	}
+
+	if conf.DataDir == nil {
+		return errors.New("data directory not configured")
+	}
+
+	if conf.Client == nil {
+		return errors.New("client configuration is missing")
+	}
+
+	if len(conf.Client.Server) == 0 {
+		return errors.New("server not configured")
+	}
+
+	if conf.Client.UserJWT == nil {
+		return errors.New("user jwt not configured")
+	}
+
+	if conf.Client.UserKey == nil {
+		return errors.New("user key not configured")
+	}
+
+	if conf.Client.TLSCACertificate == nil {
+		return errors.New("tls ca certificate not configured")
+	}
+
+	if conf.DataDir == nil {
+		return errors.New("codebase not configured")
+	}
+
+	return nil
+}
