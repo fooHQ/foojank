4ddd810c8d94d08ab530c112c75104be4d3c9675
dan <dan@foohq.io>
2025-10-25 11:51:25 +0200

    foojank/commands/config: Remove generate command

--- internal/foojank/commands/config/root.go
+++ internal/foojank/commands/config/root.go
@@ -4,16 +4,13 @@ import (
 	"github.com/urfave/cli/v3"
 
 	"github.com/foohq/foojank/internal/foojank/actions"
-	"github.com/foohq/foojank/internal/foojank/commands/config/generate"
 )
 
 func NewCommand() *cli.Command {
 	return &cli.Command{
-		Name:  "config",
-		Usage: "Manage configuration files",
-		Commands: []*cli.Command{
-			generate.NewCommand(),
-		},
+		Name:            "config",
+		Usage:           "Manage configuration files",
+		Commands:        []*cli.Command{},
 		CommandNotFound: actions.CommandNotFound,
 		OnUsageError:    actions.UsageError,
 	}
