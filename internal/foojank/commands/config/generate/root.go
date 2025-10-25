b57008bc259442aa8f038097e3443b94fbf97ef3
dan <dan@foohq.io>
2025-10-25 11:50:10 +0200

    foojank/commands/config/generate: Remove client command

--- internal/foojank/commands/config/generate/root.go
+++ internal/foojank/commands/config/generate/root.go
@@ -4,16 +4,13 @@ import (
 	"github.com/urfave/cli/v3"
 
 	"github.com/foohq/foojank/internal/foojank/actions"
-	"github.com/foohq/foojank/internal/foojank/commands/config/generate/client"
 )
 
 func NewCommand() *cli.Command {
 	return &cli.Command{
-		Name:  "generate",
-		Usage: "Generate configuration",
-		Commands: []*cli.Command{
-			client.NewCommand(),
-		},
+		Name:            "generate",
+		Usage:           "Generate configuration",
+		Commands:        []*cli.Command{},
 		CommandNotFound: actions.CommandNotFound,
 		OnUsageError:    actions.UsageError,
 	}
