db073c1818b3bbfce2b469bcd38675f1ec7b6126
dan <dan@foohq.io>
2025-10-25 13:41:03 +0200

    foojank/commands/account: Enable export, generate, list commands

--- internal/foojank/commands/account/root.go
+++ internal/foojank/commands/account/root.go
@@ -4,13 +4,20 @@ import (
 	"github.com/urfave/cli/v3"
 
 	"github.com/foohq/foojank/internal/foojank/actions"
+	"github.com/foohq/foojank/internal/foojank/commands/account/export"
+	"github.com/foohq/foojank/internal/foojank/commands/account/generate"
+	"github.com/foohq/foojank/internal/foojank/commands/account/list"
 )
 
 func NewCommand() *cli.Command {
 	return &cli.Command{
-		Name:            "account",
-		Usage:           "Manage accounts",
-		Commands:        []*cli.Command{},
+		Name:  "account",
+		Usage: "Manage accounts",
+		Commands: []*cli.Command{
+			generate.NewCommand(),
+			export.NewCommand(),
+			list.NewCommand(),
+		},
 		CommandNotFound: actions.CommandNotFound,
 		OnUsageError:    actions.UsageError,
 	}
