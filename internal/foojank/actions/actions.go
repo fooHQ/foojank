2e9b964389ca4d981fbb9849ada83fcd3957f13b
dan <dan@foohq.io>
2025-10-26 10:38:00 +0100

    foojank/actions: Rewrite configuration API
    The configuration is now stored in user-specified directory which the
    command attempts to find if the directory is not specified as a command
    argument.
    
    The configuration format has been changed from TOML to JSON.

--- internal/foojank/actions/actions.go
+++ internal/foojank/actions/actions.go
@@ -2,8 +2,11 @@ package actions
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/urfave/cli/v3"
 
@@ -11,50 +14,159 @@ import (
 	"github.com/foohq/foojank/internal/foojank/flags"
 )
 
-func NewConfig(ctx context.Context, c *cli.Command) (*config.Config, error) {
-	confDefault, err := config.NewDefault()
-	if err != nil {
-		err = fmt.Errorf("cannot create a new configuration: %w", err)
-		return nil, err
-	}
-
-	confFile, file, err := parseConfigFile(ctx, c)
-	if err != nil {
-		err = fmt.Errorf("cannot parse configuration file '%s': %w", file, err)
-		return nil, err
-	}
-
-	confFlags, err := config.ParseFlags(func(name string) (any, bool) {
+func NewConfig(_ context.Context, c *cli.Command) (*config.Config, error) {
+	confFlags, err := config.ParseFlags(c.FlagNames(), func(name string) (any, bool) {
 		return c.Value(name), c.IsSet(name)
 	})
 	if err != nil {
-		err = fmt.Errorf("cannot parse configuration flags: %w", err)
+		err = fmt.Errorf("cannot parse command options: %w", err)
 		return nil, err
 	}
 
-	result := config.Merge(confDefault, confFile, confFlags)
-	return result, nil
-}
-
-func parseConfigFile(_ context.Context, c *cli.Command) (*config.Config, string, error) {
-	var file string
-	if c.IsSet(flags.Config) {
-		file = c.String(flags.Config)
-	} else {
-		file = config.DefaultClientConfigPath
-	}
-
-	conf, err := config.ParseFile(file)
-	if err != nil {
-		// The default config file does not exist, ignore the error.
-		if os.IsNotExist(err) && !c.IsSet(flags.Config) {
-			return nil, file, nil
+	configDir, isSet := confFlags.String(flags.ConfigDir)
+	if !isSet {
+		dir, err := FindConfigDir(".")
+		if err != nil {
+			return nil, errors.New("configuration directory not found in the current directory (or any of the parent directories)")
 		}
 
-		return nil, file, err
+		configDir = dir
 	}
 
-	return conf, file, nil
+	isConfigDir, err := IsConfigDir(configDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isConfigDir {
+		err = fmt.Errorf("configuration directory not found in %q", configDir)
+		return nil, err
+	}
+
+	confFile, err := ParseConfigJson(configDir)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err = fmt.Errorf("configuration directory not found in %q", configDir)
+		} else {
+			err = fmt.Errorf("cannot parse config file: %w", err)
+		}
+		return nil, err
+	}
+
+	conf := config.Merge(newDefaultConfig(), confFile, confFlags)
+	return conf, nil
+}
+
+func newDefaultConfig() *config.Config {
+	opts := map[string]any{
+		flags.Format: "table",
+	}
+
+	if true { // TODO: check if output is tty!
+		opts[flags.NoColor] = false
+	}
+
+	return config.NewWithOptions(opts)
+}
+
+func FindConfigDir(dir string) (string, error) {
+	for i := 0; i < 128; i++ {
+		isConfigDir, err := IsConfigDir(dir)
+		if err != nil {
+			return "", err
+		}
+
+		if !isConfigDir {
+			dir = dir + "/../"
+			continue
+		}
+
+		return dir, nil
+	}
+
+	return "", errors.New("configuration directory not found")
+}
+
+func InitConfigDir(dir string) error {
+	err := os.MkdirAll(filepath.Join(dir, ".foojank"), 0755)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return err
+	}
+
+	err = InitConfigJson(dir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func IsConfigDir(dir string) (bool, error) {
+	info, err := os.Stat(filepath.Join(dir, ".foojank"))
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	if !info.IsDir() {
+		return false, nil
+	}
+
+	_, err = ParseConfigJson(dir)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, nil
+}
+
+func InitConfigJson(dir string) error {
+	pth := filepath.Join(dir, ".foojank", "config.json")
+	return os.WriteFile(pth, []byte("{}"), 0644)
+}
+
+func UpdateConfigJson(dir string, conf *config.Config) error {
+	b, err := json.Marshal(conf)
+	if err != nil {
+		return err
+	}
+
+	pth := filepath.Join(dir, ".foojank", "config.json")
+	f, err := os.CreateTemp(filepath.Dir(pth), "config*.json")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+	}()
+
+	_, err = f.Write(b)
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(f.Name(), pth)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ParseConfigJson(dir string) (*config.Config, error) {
+	pth := filepath.Join(dir, ".foojank", "config.json")
+	return config.ParseFile(pth)
 }
 
 func UsageError(ctx context.Context, c *cli.Command, err error, _ bool) error {
