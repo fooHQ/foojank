92d2b2897ae109617cc25305173ce832bc9c375a
dan <dan@foohq.io>
2025-11-22 20:41:07 +0100

    builder: Rename Build to Run
    The parameters have changed from Options to a map[string]string.

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -5,50 +5,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
-type Options struct {
-	OS                string
-	Architecture      string
-	Features          []string
-	AgentID           string
-	ServerURL         string
-	ServerCertificate string
-	UserJWT           string
-	UserKey           string
-	Stream            string
-	Consumer          string
-	InboxPrefix       string
-	ObjectStore       string
-	Custom            map[string]string
-}
-
-func Build(ctx context.Context, dir, target string, opts Options) (string, error) {
-	if opts.OS == "windows" && !strings.HasSuffix(target, ".exe") {
-		target += ".exe"
-	}
-
-	env := map[string]string{
-		"OS":                    opts.OS,
-		"ARCH":                  opts.Architecture,
-		"TARGET":                target,
-		"FEATURES":              strings.Join(opts.Features, ","),
-		"FJ_AGENT_ID":           opts.AgentID,
-		"FJ_SERVER_URL":         opts.ServerURL,
-		"FJ_SERVER_CERTIFICATE": opts.ServerCertificate,
-		"FJ_USER_JWT":           opts.UserJWT,
-		"FJ_USER_KEY":           opts.UserKey,
-		"FJ_STREAM":             opts.Stream,
-		"FJ_CONSUMER":           opts.Consumer,
-		"FJ_INBOX_PREFIX":       opts.InboxPrefix,
-		"FJ_OBJECT_STORE":       opts.ObjectStore,
-	}
-
-	for k, v := range opts.Custom {
-		env[k] = v
-	}
-
+func Run(ctx context.Context, dir string, env map[string]string) (string, error) {
 	result, err := devboxRun(ctx, dir, "build", env)
 	if err != nil {
 		return result, err
