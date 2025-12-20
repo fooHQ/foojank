650bcf7b8681164c61d73a994b1f9b37d905c4c6
dan <dan@foohq.io>
2025-12-20 17:38:59 +0100

    clients/server: Remove wrappers for JetStream methods
    The client type embeds JetStream interface so all JetStream methods are now
    exported.

--- clients/server/client.go
+++ clients/server/client.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"context"
 	"crypto/x509"
 	"os"
 	"strings"
@@ -9,11 +8,10 @@ import (
 
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/nats.go/jetstream"
-	"github.com/nats-io/nuid"
 )
 
 type Client struct {
-	js jetstream.JetStream
+	jetstream.JetStream
 }
 
 func New(
@@ -28,133 +26,8 @@ func New(
 	}
 
 	return &Client{
-		js: js,
+		JetStream: js,
 	}, nil
-}
-
-func (c *Client) Publish(ctx context.Context, msg *nats.Msg) error {
-	if msg.Header == nil {
-		msg.Header = make(nats.Header)
-	}
-	msg.Header.Set(nats.MsgIdHdr, nuid.Next())
-	_, err := c.js.PublishMsg(ctx, msg)
-	return err
-}
-
-func (c *Client) CreateStream(ctx context.Context, name string, subjects []string) (jetstream.Stream, error) {
-	_, err := c.js.CreateStream(ctx, jetstream.StreamConfig{
-		Name:     name,
-		Subjects: subjects,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return nil, nil
-}
-
-func (c *Client) ListStreams(ctx context.Context) ([]string, error) {
-	var names []string
-	for stream := range c.js.ListStreams(ctx).Info() {
-		if stream == nil {
-			break
-		}
-		names = append(names, stream.Config.Name)
-	}
-	return names, nil
-}
-
-func (c *Client) DeleteStream(ctx context.Context, name string) error {
-	err := c.js.DeleteStream(ctx, name)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Client) CreateConsumer(ctx context.Context, stream string, subjects []string) (jetstream.Consumer, error) {
-	consumer, err := c.js.CreateConsumer(ctx, stream, jetstream.ConsumerConfig{
-		DeliverPolicy:  jetstream.DeliverAllPolicy,
-		AckPolicy:      jetstream.AckExplicitPolicy,
-		MaxAckPending:  1,
-		FilterSubjects: subjects,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return consumer, nil
-}
-
-func (c *Client) CreateDurableConsumer(ctx context.Context, stream, name string, subjects []string) (jetstream.Consumer, error) {
-	consumer, err := c.js.CreateConsumer(ctx, stream, jetstream.ConsumerConfig{
-		Durable:        name,
-		DeliverPolicy:  jetstream.DeliverLastPolicy,
-		AckPolicy:      jetstream.AckExplicitPolicy,
-		MaxAckPending:  1,
-		FilterSubjects: subjects,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return consumer, nil
-}
-
-func (c *Client) DeleteConsumer(ctx context.Context, stream, name string) error {
-	err := c.js.DeleteConsumer(ctx, stream, name)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Client) CreateObjectStore(ctx context.Context, name, description string) error {
-	_, err := c.js.CreateObjectStore(ctx, jetstream.ObjectStoreConfig{
-		Bucket:      name,
-		Description: description,
-	})
-	if err != nil {
-		return &errorObjectStore{err}
-	}
-	return nil
-}
-
-func (c *Client) DeleteObjectStore(ctx context.Context, name string) error {
-	err := c.js.DeleteObjectStore(ctx, name)
-	if err != nil {
-		return &errorObjectStore{err}
-	}
-	return nil
-}
-
-func (c *Client) ListObjectStores(ctx context.Context) ([]*ObjectStore, error) {
-	var result []*ObjectStore
-	for name := range c.js.ObjectStoreNames(ctx).Name() {
-		store, err := c.js.ObjectStore(ctx, name)
-		if err != nil {
-			return nil, &errorObjectStore{err}
-		}
-
-		s, err := NewObjectStore(ctx, store)
-		if err != nil {
-			return nil, err
-		}
-
-		result = append(result, s)
-	}
-	return result, nil
-}
-
-func (c *Client) GetObjectStore(ctx context.Context, name string) (*ObjectStore, error) {
-	store, err := c.js.ObjectStore(ctx, name)
-	if err != nil {
-		return nil, &errorObjectStore{err}
-	}
-
-	s, err := NewObjectStore(ctx, store)
-	if err != nil {
-		return nil, err
-	}
-
-	return s, nil
 }
 
 func connect(
