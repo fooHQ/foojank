f24c84c6f38f3ec003f54565c35c2d2b41f2c012
dan <dan@foohq.io>
2025-12-26 20:13:20 +0100

    proto: Refactor package structure
    Changes how the code is laid out in the directory.

---
+++ proto/update_worker_stdio.go
@@ -0,0 +1,47 @@
+package proto
+
+import "github.com/foohq/foojank/proto/capnp"
+
+type UpdateWorkerStdio struct {
+	Data []byte
+}
+
+func marshalUpdateWorkerStdio(data UpdateWorkerStdio) ([]byte, error) {
+	msg, err := newMessage()
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := capnp.NewUpdateWorkerStdio(msg.Segment())
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.SetData(data.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	err = msg.Content().SetUpdateWorkerStdio(m)
+	if err != nil {
+		return nil, err
+	}
+
+	return msg.Message().Marshal()
+}
+
+func unmarshalUpdateWorkerStdio(message capnp.Message) (UpdateWorkerStdio, error) {
+	v, err := message.Content().UpdateWorkerStdio()
+	if err != nil {
+		return UpdateWorkerStdio{}, err
+	}
+
+	data, err := v.Data()
+	if err != nil {
+		return UpdateWorkerStdio{}, err
+	}
+
+	return UpdateWorkerStdio{
+		Data: data,
+	}, nil
+}
