314752bcfa722741ac68e5a6353d58f43abe4d9e
dan <dan@foohq.io>
2026-01-02 11:22:21 +0100

    commands/account/export: Add --user to export user JWT

--- internal/commands/account/export/command.go
+++ internal/commands/account/export/command.go
@@ -12,13 +12,20 @@ import (
 	"github.com/foohq/foojank/internal/actions"
 	"github.com/foohq/foojank/internal/auth"
 	"github.com/foohq/foojank/internal/config"
+	"github.com/foohq/foojank/internal/flags"
 )
 
 func NewCommand() *cli.Command {
 	return &cli.Command{
-		Name:            "export",
-		ArgsUsage:       "<account-name>",
-		Usage:           "Export account's JWT",
+		Name:      "export",
+		ArgsUsage: "<account-name>",
+		Usage:     "Export JWT",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  flags.User,
+				Usage: "export user JWT",
+			},
+		},
 		Before:          before,
 		Action:          action,
 		OnUsageError:    actions.UsageError,
@@ -44,7 +51,10 @@ func before(ctx context.Context, c *cli.Command) (context.Context, error) {
 }
 
 func action(ctx context.Context, c *cli.Command) error {
+	conf := actions.GetConfigFromContext(ctx)
 	logger := actions.GetLoggerFromContext(ctx)
+
+	userJWT, _ := conf.Bool(flags.User)
 
 	if c.Args().Len() < 1 {
 		logger.ErrorContext(ctx, "Command expects the following arguments: %s", c.ArgsUsage)
@@ -53,15 +63,39 @@ func action(ctx context.Context, c *cli.Command) error {
 
 	name := c.Args().First()
 
-	accountJWT, _, err := auth.ReadAccount(name)
+	var (
+		s   string
+		err error
+	)
+	if userJWT {
+		s, err = exportUserJWT(name)
+	} else {
+		s, err = exportAccountJWT(name)
+	}
 	if err != nil {
-		logger.ErrorContext(ctx, "Cannot read account %q: %v", name, err)
+		logger.ErrorContext(ctx, "Cannot export JWT: %v", err)
 		return err
 	}
 
-	_, _ = fmt.Fprintf(os.Stdout, "%s\n", accountJWT)
+	_, _ = fmt.Fprintf(os.Stdout, "%s\n", s)
 
 	return nil
+}
+
+func exportUserJWT(name string) (string, error) {
+	userJWT, _, err := auth.ReadUser(name)
+	if err != nil {
+		return "", err
+	}
+	return userJWT, nil
+}
+
+func exportAccountJWT(name string) (string, error) {
+	accountJWT, _, err := auth.ReadAccount(name)
+	if err != nil {
+		return "", err
+	}
+	return accountJWT, nil
 }
 
 func validateConfiguration(conf *config.Config) error {
