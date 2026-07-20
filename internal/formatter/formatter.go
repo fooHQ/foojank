3cef926f1714b974f821618d0d07c2df90281829
dan <dan@foohq.io>
2026-07-20 07:34:44 +0200

    formatter: Add WithSortByColumn and SetHeader
    JSON formatter skips the header.

--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -1,6 +1,7 @@
 package formatter
 
 import (
+	"cmp"
 	"io"
 	"slices"
 )
@@ -11,11 +12,20 @@ const (
 )
 
 type Table struct {
-	rows [][]Cell
+	header []Cell
+	rows   [][]Cell
 }
 
 func NewTable() *Table {
 	return &Table{}
+}
+
+func (t *Table) SetHeader(data []Cell) {
+	t.header = slices.Clone(data)
+}
+
+func (t *Table) Header() []Cell {
+	return t.header
 }
 
 func (t *Table) AddRow(data []Cell) {
@@ -52,7 +62,8 @@ func NewFormatter(format string, opt ...Option) Formatter {
 }
 
 type options struct {
-	NoColor bool
+	NoColor       bool
+	SortByColumns []int
 }
 
 type Option func(*options)
@@ -61,4 +72,68 @@ func WithNoColor(noColor bool) Option {
 	return func(opts *options) {
 		opts.NoColor = noColor
 	}
+}
+
+func WithSortByColumn(c ...int) Option {
+	return func(opts *options) {
+		opts.SortByColumns = c
+	}
+}
+
+func sortedRows(table *Table, opts options) [][]Cell {
+	rows := table.Rows()
+	if len(opts.SortByColumns) == 0 {
+		return rows
+	}
+
+	sorted := slices.Clone(rows)
+	slices.SortStableFunc(sorted, func(a, b []Cell) int {
+		for _, col := range opts.SortByColumns {
+			if c := compareCells(cellAt(a, col), cellAt(b, col)); c != 0 {
+				return c
+			}
+		}
+		return 0
+	})
+	return sorted
+}
+
+func cellAt(row []Cell, c int) Cell {
+	if c < 0 || c >= len(row) {
+		return nil
+	}
+	return row[c]
+}
+
+// compareCells orders two cells, using their underlying values so numeric and
+// time columns sort naturally rather than lexically. Cells of differing or
+// unknown types fall back to comparing their string representation.
+func compareCells(a, b Cell) int {
+	switch av := a.(type) {
+	case IntCell:
+		if bv, ok := b.(IntCell); ok {
+			return cmp.Compare(av.Value(), bv.Value())
+		}
+	case UintCell:
+		if bv, ok := b.(UintCell); ok {
+			return cmp.Compare(av.Value(), bv.Value())
+		}
+	case SizeCell:
+		if bv, ok := b.(SizeCell); ok {
+			return cmp.Compare(av.Value(), bv.Value())
+		}
+	case TimeCell:
+		if bv, ok := b.(TimeCell); ok {
+			return av.Value().Compare(bv.Value())
+		}
+	}
+
+	var as, bs string
+	if a != nil {
+		as = a.String()
+	}
+	if b != nil {
+		bs = b.String()
+	}
+	return cmp.Compare(as, bs)
 }
