b22f142d61862604cf94b12e428657749f4967c3
dan <dan@foohq.io>
2026-07-22 21:10:39 +0200

    commands/account/list: Use SetHeader

--- internal/commands/account/list/command.go
+++ internal/commands/account/list/command.go
@@ -64,7 +64,7 @@ func action(ctx context.Context, c *cli.Command) error {
 	}
 
 	table := formatter.NewTable()
-	table.AddRow([]formatter.Cell{
+	table.SetHeader([]formatter.Cell{
 		formatter.NewStringCell("NAME").WithBold(),
 		formatter.NewStringCell("ACCOUNT ID").WithBold(),
 		formatter.NewStringCell("ISSUED AT").WithBold(),
@@ -84,7 +84,10 @@ func action(ctx context.Context, c *cli.Command) error {
 		})
 	}
 
-	err = formatter.NewFormatter(format, formatter.WithNoColor(noColor)).Write(os.Stdout, table)
+	err = formatter.NewFormatter(
+		format,
+		formatter.WithNoColor(noColor),
+	).Write(os.Stdout, table)
 	if err != nil {
 		logger.ErrorContext(ctx, "Cannot write formatted output: %v", err)
 		return err
