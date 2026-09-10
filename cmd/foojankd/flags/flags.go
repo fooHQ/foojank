5bda4530459fc20dcc14602aba7a36b0cc6aa6da
dan <dan@foohq.io>
2026-09-10 08:02:19 +0200

    cmd: Add foojankd

---
+++ cmd/foojankd/flags/flags.go
@@ -0,0 +1,14 @@
+package flags
+
+const (
+	ServerURL         = "server-url"
+	ServerCertificate = "server-certificate"
+	Account           = "account"
+	Format            = "format"
+	Name              = "name"
+	NoColor           = "no-color"
+	LinkAccount       = "link-account"
+	UnlinkAccount     = "unlink-account"
+	AcceptLinkFrom    = "accept-link-from"
+	RemoveLink        = "remove-link"
+)
