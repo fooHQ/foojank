bce6fb90ca271382b16ffbc431e379daf866d25f
dan <dan@foohq.io>
2026-09-20 06:54:21 +0200

    cmd/foojank: Move root command definition inside the commands package

---
+++ cmd/foojank/commands/root.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"github.com/urfave/cli/v3"
+
+	"github.com/foohq/foojank"
+	"github.com/foohq/foojank/cmd/foojank/actions"
+	"github.com/foohq/foojank/cmd/foojank/commands/account"
+	"github.com/foohq/foojank/cmd/foojank/commands/agent"
+	"github.com/foohq/foojank/cmd/foojank/commands/config"
+	"github.com/foohq/foojank/cmd/foojank/commands/gateway"
+	"github.com/foohq/foojank/cmd/foojank/commands/job"
+	"github.com/foohq/foojank/cmd/foojank/commands/profile"
+	"github.com/foohq/foojank/cmd/foojank/commands/storage"
+	"github.com/foohq/foojank/cmd/foojank/flags"
+)
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "foojank",
+		Usage:   "Command and control framework",
+		Version: foojank.Version(),
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  flags.NoColor,
+				Usage: "disable color output",
+			},
+		},
+		Commands: []*cli.Command{
+			account.NewCommand(),
+			agent.NewCommand(),
+			config.NewCommand(),
+			job.NewCommand(),
+			profile.NewCommand(),
+			storage.NewCommand(),
+			gateway.NewCommand(),
+		},
+		CommandNotFound:       actions.CommandNotFound,
+		OnUsageError:          actions.UsageError,
+		HideHelpCommand:       true,
+		EnableShellCompletion: true,
+	}
+}
--- cmd/foojank/main.go
+++ cmd/foojank/main.go
@@ -5,48 +5,12 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/urfave/cli/v3"
-
-	"github.com/foohq/foojank"
-	"github.com/foohq/foojank/cmd/foojank/actions"
-	"github.com/foohq/foojank/cmd/foojank/commands/account"
-	"github.com/foohq/foojank/cmd/foojank/commands/agent"
-	"github.com/foohq/foojank/cmd/foojank/commands/config"
-	"github.com/foohq/foojank/cmd/foojank/commands/gateway"
-	"github.com/foohq/foojank/cmd/foojank/commands/job"
-	"github.com/foohq/foojank/cmd/foojank/commands/profile"
-	"github.com/foohq/foojank/cmd/foojank/commands/storage"
-	"github.com/foohq/foojank/cmd/foojank/flags"
+	"github.com/foohq/foojank/cmd/foojank/commands"
 )
-
-var app = &cli.Command{
-	Name:    "foojank",
-	Usage:   "Command and control framework",
-	Version: foojank.Version(),
-	Flags: []cli.Flag{
-		&cli.BoolFlag{
-			Name:  flags.NoColor,
-			Usage: "disable color output",
-		},
-	},
-	Commands: []*cli.Command{
-		account.NewCommand(),
-		agent.NewCommand(),
-		config.NewCommand(),
-		job.NewCommand(),
-		profile.NewCommand(),
-		storage.NewCommand(),
-		gateway.NewCommand(),
-	},
-	CommandNotFound:       actions.CommandNotFound,
-	OnUsageError:          actions.UsageError,
-	HideHelpCommand:       true,
-	EnableShellCompletion: true,
-}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	err := app.Run(ctx, os.Args)
+	err := commands.NewCommand().Run(ctx, os.Args)
 	if err != nil {
 		cancel()
 		os.Exit(1)
