effbb0a70b791c8e8fb5783f3330a1b545e1142f
dan <dan@foohq.io>
2026-09-25 06:15:59 +0200

    cmd/foojankd: Add start command

---
+++ cmd/foojankd/commands/start/command.go
@@ -0,0 +1,218 @@
+package start
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats.go/jetstream"
+	"github.com/nats-io/nkeys"
+	"github.com/urfave/cli/v3"
+
+	"github.com/foohq/foojank"
+	"github.com/foohq/foojank/cmd/foojankd/actions"
+	"github.com/foohq/foojank/cmd/foojankd/flags"
+	"github.com/foohq/foojank/internal/auth"
+	"github.com/foohq/foojank/internal/authdir"
+	"github.com/foohq/foojank/internal/clients/server"
+	"github.com/foohq/foojank/internal/config"
+	"github.com/foohq/foojank/internal/consumer"
+	"github.com/foohq/foojank/internal/directory"
+	"github.com/foohq/foojank/internal/foojankd"
+	"github.com/foohq/foojank/internal/handler"
+	"github.com/foohq/foojank/internal/publisher"
+	protoagent "github.com/foohq/foojank/proto/agent"
+)
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:    "start",
+		Usage:   "Start the daemon",
+		Version: foojank.Version(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  flags.ServerURL,
+				Usage: "set server URL",
+			},
+			&cli.StringFlag{
+				Name:      flags.ServerCertificate,
+				Usage:     "set path to server's certificate",
+				TakesFile: true,
+			},
+			&cli.StringFlag{
+				Name:  flags.Account,
+				Usage: "set account name",
+			},
+			&cli.BoolFlag{
+				Name:  flags.NoColor,
+				Usage: "disable color output",
+			},
+		},
+		Before:                before,
+		Action:                action,
+		CommandNotFound:       actions.CommandNotFound,
+		OnUsageError:          actions.UsageError,
+		HideHelpCommand:       true,
+		EnableShellCompletion: true,
+	}
+}
+
+func before(ctx context.Context, c *cli.Command) (context.Context, error) {
+	ctx, err := actions.LoadConfig(os.Stderr, validateConfiguration)(ctx, c)
+	if err != nil {
+		return ctx, err
+	}
+
+	ctx, err = actions.SetupLogger(os.Stderr)(ctx, c)
+	if err != nil {
+		return ctx, err
+	}
+
+	return ctx, nil
+}
+
+const (
+	streamName   = "event-stream"
+	consumerName = "event-consumer"
+)
+
+func action(ctx context.Context, c *cli.Command) error {
+	conf := actions.GetConfigFromContext(ctx)
+	logger := actions.GetLoggerFromContext(ctx)
+
+	serverURL, _ := conf.String(flags.ServerURL)
+	serverCert, _ := conf.String(flags.ServerCertificate)
+	accountName, _ := conf.String(flags.Account)
+
+	accountKey, err := authdir.GetAccountKey(accountName)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot read account key: %v", err)
+		return err
+	}
+
+	userJWT, userKey, err := generateUserCreds(accountName, accountKey)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot generate user credentials: %v", err)
+		return err
+	}
+
+	srv, err := server.New([]string{serverURL}, userJWT, userKey, serverCert)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot connect to the server: %v", err)
+		return err
+	}
+
+	_, err = srv.CreateStream(ctx, jetstream.StreamConfig{
+		Name: streamName,
+		Subjects: []string{
+			protoagent.CmdStartWorkerSubject("*", "*", "*"),
+			protoagent.CmdStopWorkerSubject("*", "*", "*"),
+			protoagent.CmdWriteStdinSubject("*", "*", "*"),
+			protoagent.EvtStartWorkerSubject("*", "*", "*"),
+			protoagent.EvtStopWorkerSubject("*", "*", "*"),
+			protoagent.EvtWorkerStdoutSubject("*", "*", "*"),
+			protoagent.EvtWorkerStatusSubject("*", "*", "*"),
+			protoagent.EvtAgentInfoSubject("*", "*"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = srv.CreateConsumer(ctx, streamName, jetstream.ConsumerConfig{
+		Durable:       consumerName,
+		DeliverPolicy: jetstream.DeliverLastPolicy,
+		AckPolicy:     jetstream.AckExplicitPolicy,
+		MaxAckPending: 1,
+	})
+	if err != nil {
+		return err
+	}
+
+	agentDir, err := directory.OpenAgentDirectory(ctx, srv)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot open agent directory: %v", err)
+		return err
+	}
+
+	gatewayDir, err := directory.OpenGatewayDirectory(ctx, srv)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot open gateway directory: %v", err)
+		return err
+	}
+
+	userDir, err := directory.OpenUserDirectory(ctx, srv)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot open user directory: %v", err)
+		return err
+	}
+
+	err = foojankd.New(logger, foojankd.Config{
+		Consumer: consumer.NewNATSConsumer(logger, consumer.NATSConsumerConfig{
+			Connection: srv.Conn(),
+		}),
+		Handler: handler.NewNATSHandler(logger, handler.NATSHandlerConfig{
+			Connection:       srv,
+			AgentDirectory:   agentDir,
+			GatewayDirectory: gatewayDir,
+			UserDirectory:    userDir,
+			AccountKey:       accountKey,
+			Stream:           streamName,
+		}),
+		Publisher: publisher.NewNATSPublisher(logger, publisher.NATSPublisherConfig{
+			Connection: srv.Conn(),
+		}),
+	}).Start(ctx)
+	if err != nil {
+		logger.ErrorContext(ctx, "Cannot start daemon: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func generateUserCreds(name string, accountKey nkeys.KeyPair) (string, string, error) {
+	userKey, err := auth.NewUserKey()
+	if err != nil {
+		return "", "", err
+	}
+
+	userClaims, err := auth.NewUserJWT(name, jwt.Permissions{}, userKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	userJWT, err := userClaims.Encode(accountKey)
+	if err != nil {
+		return "", "", err
+	}
+
+	userSeed, err := userKey.Seed()
+	if err != nil {
+		return "", "", err
+	}
+
+	return userJWT, string(userSeed), nil
+}
+
+func validateConfiguration(conf *config.Config) error {
+	for _, opt := range []string{
+		flags.ServerURL,
+		flags.Account,
+	} {
+		switch opt {
+		case flags.ServerURL:
+			v, ok := conf.String(opt)
+			if !ok || v == "" {
+				return errors.New("server URL not configured")
+			}
+		case flags.Account:
+			v, ok := conf.String(opt)
+			if !ok || v == "" {
+				return errors.New("account not configured")
+			}
+		}
+	}
+	return nil
+}
